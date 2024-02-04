6b19737ff38bab0dbf186fc95467d012c78af076
Arvind Mali <arvindmali5768@gmail.com>
2024-02-04 18:06:06 +0530

    path updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, '/')));
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
 let port = 5000;
